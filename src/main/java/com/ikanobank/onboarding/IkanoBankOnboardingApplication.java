d7ba3bce46e7d9519bdba31d719425257500f97a
sandhya <sandhya.machani47@gmail.com>
2026-07-24 22:33:46 +0200

    added springboot main class

---
+++ src/main/java/com/ikanobank/onboarding/IkanoBankOnboardingApplication.java
@@ -0,0 +1,7 @@
+package com.ikanobank.onboarding;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+@SpringBootApplication
+public class IkanoBankOnboardingApplication {
+  public static void main(String[] args) { SpringApplication.run(IkanoBankOnboardingApplication.class, args); }
+}
